faacdc878be2c8d660e51830a8c6900feaac2340
Guany <1084072690@qq.com>
2023-11-12 14:45:31 +0800

    chore: Add repository and module fields to package.json, update test file

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,8 @@
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { sum } from '../src'
 
-it('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3)
+describe('unbuild-starter', () => {
+  it('adds 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3)
+  })
 })
