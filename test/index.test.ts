f9476b86a5df05903e88292cb9a319ff3374ff9a
Guany <1084072690@qq.com>
2023-11-14 14:03:20 +0800

    chore: update

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,8 @@
 import { describe, expect, it } from 'vitest'
-import { sum } from '../src'
+import {} from '../src'
 
 describe('unbuild-starter', () => {
-  it('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
+  it.todo('pass', () => {
+    expect(true).toBe(true)
   })
 })
